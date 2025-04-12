b24ad6f95a9551cfbfc3c7fc102944edd98017fa
Pongstrider <pongstrider@gmail.com>
2025-04-12 22:17:17 +0200

    Bluetooth to App data implementation
    The Arduino now stores its current LED values and will assume those values upon startup.
    Furthermore, upon connecting the Autolight app to the light, the Arduino will send a data package with its current brightness and color temperature values.
    The app will then update its on-screen sliders to match the values received. before allowing further user input.

--- AutoLight.ino
+++ AutoLight.ino
@@ -1,4 +1,6 @@
 #include <SoftwareSerial.h>
+#include <EEPROM.h>
+
 SoftwareSerial btSerial(3,2);
 
 void adjustLED(int newBrightness, int warmthAmount);
@@ -7,48 +9,56 @@ void executeCommand(String commandString);
 void setup() {
   Serial.begin(9600); //open the serial port
   btSerial.begin(9600); // open the bluetooth serial port
+
+  adjustLED(EEPROM.read(4), EEPROM.read(5));
 }
 
 void loop() {
-  // Check if data is available from Bluetooth and send it to the Serial Monitor
-  if (btSerial.available()) {
-    Serial.println(btSerial.readString()); // Send from Bluetooth to Serial
-  }
-
-  // Check if data is available from Serial Monitor and send it to Bluetooth
-  if (Serial.available()) {
+if (Serial.available()) {
     String serialInput = Serial.readStringUntil('\n'); // Read from Serial Monitor
-    btSerial.println(serialInput); // Send it to Bluetooth
     executeCommand(serialInput); // Execute the command if needed
   }
+
 }
 
 void executeCommand(String commandString){
-
   int commaFirst = commandString.indexOf(',');
   int commaSecond = commandString.indexOf(',', commaFirst + 1);
-
   String functionToCall = commandString.substring(0, commaFirst);
-  String parameter1 = commandString.substring(commaFirst + 1, commaSecond);
-  String parameter2 = commandString.substring(commaSecond + 1);
 
-  Serial.print(functionToCall);
+  if(functionToCall.startsWith("readMemory")){
+    Serial.print(EEPROM.read(4));
+    Serial.print(",");
+    Serial.println(EEPROM.read(5));
+  }
 
+  int parameter1 = commandString.substring(commaFirst + 1, commaSecond).toInt();
+  int parameter2 = commandString.substring(commaSecond + 1).toInt();
+
+  
+    
   if(functionToCall.startsWith("setWarmth")){
-    adjustLED(parameter1.toInt(), parameter2.toInt());
+    if(parameter1 >= 0 && parameter1 <= 255 && parameter2 >= 0 && parameter2 <= 10){
+      adjustLED(parameter1, parameter2);
+    } 
   }
 }
 
-void adjustLED(int newBrightness = 255, int warmthAmount = 0){
+void adjustLED(const int newBrightness = 255, const int warmthAmount = 0){
+  EEPROM.write(4, newBrightness);
+  EEPROM.write(5, warmthAmount);
+
   int newGreen = newBrightness;
   int newRed = newBrightness;
   int newBlue = newBrightness;
 
-  if(warmthAmount < 0){
-    newRed = newBrightness + warmthAmount*(newBrightness/5);
-  } else if (warmthAmount > 0){
-    newBlue = newBrightness - warmthAmount*(newBrightness/5);
-    newGreen = newBrightness - warmthAmount*(newBrightness/8);
+  int adjustedWarmth = warmthAmount - 5;
+
+  if(warmthAmount < 5){
+    newRed = newBrightness + adjustedWarmth*(newBrightness/5);
+  } else if (warmthAmount > 5){
+    newBlue = newBrightness - adjustedWarmth*(newBrightness/5);
+    newGreen = newBrightness - adjustedWarmth*(newBrightness/8);
   }
   
   analogWrite(9, newRed);
